b7b456be0cfb5c48c909c1d2ee731e623269a0dd
reinerkp <kate.reiner1@gmail.com>
2023-05-13 12:44:07 -0600

    added routes to server

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 
 const express = require("express");
-const path = require("path");
-const fs = require("fs");
+const apiRoutes = require("./Routes/apiroutes");
+const htmlRoutes = require ("./Routes/htmlroutes");
 
 const PORT = process.env.PORT || 3001;
 
-const noteData = require("./db/db.json");
-const { response } = require("express");
+
 
 const app = express();
 
@@ -16,52 +15,38 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
+app.use("/api", apiRoutes);
+app.use("/",htmlRoutes);
 
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./public/index.html"));
+// });
+
+// app.get("/notes", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./public/notes.html"));
+// });
 
 app.listen(PORT, () => {
   console.log(`The server is listening on PORT: ${PORT}`);
 });
 
 
-app.post("/api/notes", (req, res) => {
-  const newNote = req.body;
-  newNote.id = Date.now().toString();
-  noteData.push(newNote);
-  fs.writeFile("./db/db.json", JSON.stringify(noteData), (err) => {
-    if (err) throw err;
-    res.json(newNote);
-  });
-});
+// app.post("/api/notes", (req, res) => {
+//   const newNote = req.body;
+//   newNote.id = Date.now().toString();
+//   noteData.push(newNote);
+//   fs.writeFile("./db/db.json", JSON.stringify(noteData), (err) => {
+//     if (err) throw err;
+//     res.json(newNote);
+//   });
+// });
 
 
-app.get("/api/notes", (req, res) => {
-  fs.readFile("./db/db.json", JSON.parse(noteData), (err) => {
-    if (err) throw err;
-    res.json(noteData);
-  });
-});
+// app.get("/api/notes", (req, res) => {
+//   fs.readFile("./db/db.json", JSON.parse(noteData), (err) => {
+//     if (err) throw err;
+//     res.json(noteData);
+//   });
+// });
 // document.getElementById("save-note").addEventListener("click", function(event){
 
-// event.preventDefault();
-// const noteTitle = document.getElementById("note-title").value; 
-// const noteText = document.getElementById("note-text").value; 
-// const newNote = { title: noteTitle, Text: noteText };
-// fetch("/api/notes", {
-//   method: "POST",
-//   headers: {
-//     "Content-Type": "application/json"
-//   },
-//   body: JSON.stringify(newNote)
-// })
-// .then(response => response.json())
-// .then(data => {
-//   console.log(data);
-// })
-// .catch(error => console.error(error));
-// })
